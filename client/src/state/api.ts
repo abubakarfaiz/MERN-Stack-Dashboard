ed6a9709264dc4d2a06ee841d6ed9e9f32f9b9f9
Abu Bakar Faiz <163720857+abubakarfaiz@users.noreply.github.com>
2024-09-12 15:43:26 +0500

    Update api.ts

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -6,7 +6,7 @@ import {
 } from "./types";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.https://mern-stack-dashboard-s67i.vercel.app/ }),
   reducerPath: "main",
   tagTypes: ["Kpis", "Products", "Transactions"],
   endpoints: (build) => ({
